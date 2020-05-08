3930c78df80d65d5a030c7bf97122b041a97b8e2
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2020-05-08 20:11:24 +0200

    Update and rename uppercaser.lua to uppercaser.xml

--- uppercaser.lua
+++
@@ -1,9 +0,0 @@
-function text_prompt(msg)
-  io.write(msg)
-  io.flush()
-  return io.read()
-end
-
-
-usrinput = text_prompt('Please choose your Word you want to be displayed in UPPERCASE')
-print(string.upper(usrinput))
